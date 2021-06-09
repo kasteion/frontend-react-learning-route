5826facfcbf18b0e0534a3d8ce56a743e48ae7c6
Fredy Augusto Castellón Bedoya <castellonbedoya@gmail.com>
2021-06-08 21:06:46 -0600

    structure of components

---
+++ 12-react-hooks/plati-conf-merch/src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "../containers/Home";
+export { default as Checkout } from "../containers/Checkout";
+export { default as Information } from "../containers/Information";
+export { default as Payment } from "../containers/Payment";
+export { default as Success } from "../containers/Success";
+export { default as NotFound } from "../containers/NotFound";
--- 12-react-hooks/plati-conf-merch/webpack.config.js
+++ 12-react-hooks/plati-conf-merch/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
@@ -49,6 +50,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
+    historyApiFallback: true,
     port: 3005,
   },
 };
